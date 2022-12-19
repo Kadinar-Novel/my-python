bac1b70fc8555e7f43f83b3baddffdad946c42a0
Kadinar-Novel <kadinarnvl@gmail.com>
2022-12-19 15:09:05 +0700

    Looping and condition

--- dasar_sintaksi_perulangan.py
+++ dasar_sintaksi_perulangan.py
@@ -22,4 +22,8 @@ print(f"Jumlah buku: {jumlah_buku_while}")
 jumlah_buku_while_yang_sudah_dibaca = 0
 while jumlah_buku_while_yang_sudah_dibaca < jumlah_buku_while:
     jumlah_buku_while_yang_sudah_dibaca = jumlah_buku_while_yang_sudah_dibaca + 1
-    print(f"Pake while Buku ke {jumlah_buku_while_yang_sudah_dibaca} sudah dibaca")
+    if jumlah_buku_while_yang_sudah_dibaca == 5:
+        print(f"Buku ke {jumlah_buku_while_yang_sudah_dibaca} ini sulit dibaca")
+        break
+    else:
+        print(f"Pake while Buku ke {jumlah_buku_while_yang_sudah_dibaca} sudah dibaca")
